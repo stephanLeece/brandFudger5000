13558af0cfdba23dfc4fa1ee3b8edc28be33424a
stephanLeece <stephan.leece@outlook.com>
2020-06-27 23:42:25 +0200

    init

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    consumer_key: "VFT9xtog5QK49TGZE01c3YI7F",
+    consumer_secret: "DCuB6o6SlIKkPhJ6mJ22j3AovTS0zc6FkvYS1KzqrA1lN1if5S",
+    access_token: "1270094135081533440-dKqNwODDVNeotRoAPB4FHZw9RHsqVZ",
+    access_token_secret: "eg28z9QNXc3Hr0LOz0ni36d5LmCIPhjHmURnjBD6VjR0g",
+};
